3f10878a2e9dc1690d2edd508ec2b05ab31321de
Makerov <m.a.makerov@gmail.com>
2026-04-28 22:27:36 +0600

    Export patient bot builders

--- app/bots/__init__.py
+++ app/bots/__init__.py
@@ -1,1 +1,5 @@
 """Bot adapter package."""
+
+from app.bots.patient_bot import build_patient_bot, build_patient_dispatcher, build_patient_router
+
+__all__ = ["build_patient_bot", "build_patient_dispatcher", "build_patient_router"]
