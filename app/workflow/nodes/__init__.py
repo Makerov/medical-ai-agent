0e9b445f68afeec41ea494a8a947ab5756e6798d
Makerov <m.a.makerov@gmail.com>
2026-04-30 20:16:06 +0600

    Export indicator extraction node

--- app/workflow/nodes/__init__.py
+++ app/workflow/nodes/__init__.py
@@ -1,5 +1,6 @@
 """Workflow nodes package."""
 
+from app.workflow.nodes.extract_indicators import ExtractIndicatorsNode
 from app.workflow.nodes.parse_document import ParseDocumentNode
 
-__all__ = ["ParseDocumentNode"]
+__all__ = ["ExtractIndicatorsNode", "ParseDocumentNode"]
