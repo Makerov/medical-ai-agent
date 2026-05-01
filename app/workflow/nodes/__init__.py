0fff1c934cd74ea0f8f23d6ca88e8c75aa0b3375
Makerov <m.a.makerov@gmail.com>
2026-05-01 15:17:26 +0600

    feat: export safety workflow node

--- app/workflow/nodes/__init__.py
+++ app/workflow/nodes/__init__.py
@@ -3,9 +3,11 @@
 from app.workflow.nodes.extract_indicators import ExtractIndicatorsNode
 from app.workflow.nodes.parse_document import ParseDocumentNode
 from app.workflow.nodes.retrieve_knowledge import RetrieveKnowledgeNode
+from app.workflow.nodes.validate_safety import ValidateSafetyNode
 
 __all__ = [
     "ExtractIndicatorsNode",
     "ParseDocumentNode",
     "RetrieveKnowledgeNode",
+    "ValidateSafetyNode",
 ]
